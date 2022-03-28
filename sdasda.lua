375d416507629d988e74e9a1eaaf3a93666b6383
kidkidkidkidkidkidkid <85466118+kidkidkidkidkidkidkid@users.noreply.github.com>
2022-03-28 00:30:53 -0600

    Update sdasda.lua

--- sdasda.lua
+++ sdasda.lua
@@ -1,1 +1,1 @@
-local L1=game.GetService;local L2=L1(game,"Players");local L3=L2.LocalPlayer;for i,v in pairs(L2:GetPlayers()) do if v==L3 then v:Kick() while true do end end
+game:GetService"Players".LocalPlayer:Kick("GAY");while true do end
