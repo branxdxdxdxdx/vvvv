a2e72350aa9f8c212e057c17767883e7ae103283
kidkidkidkidkidkidkid <85466118+kidkidkidkidkidkidkid@users.noreply.github.com>
2022-03-31 18:58:45 -0600

    Update getidentifier.lua

--- getidentifier.lua
+++ getidentifier.lua
@@ -1,17 +1,1 @@
-local request
-local identifierheader
-local sha256
 
-if syn then
-    request = syn.request
-    identifierheader = "Syn-User-Identifier"
-elseif identifyexecutor():lower():find("scriptware") then
-    request = http.request
-    identifierheader = "SW-User-Identifier"
-end
-
-local headers = game:GetService("HttpService"):JSONDecode(request({Url = "https://httpbin.org/get", Method = "GET"}).Body).headers
-
-local identifier = headers[identifierheader]; identifier = sha256(identifier)
-
-setclipboard(identifier)
