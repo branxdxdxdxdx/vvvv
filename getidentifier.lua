3a0da340116180043616c9ec80ce4d799dbc634c
kidkidkidkidkidkidkid <85466118+kidkidkidkidkidkidkid@users.noreply.github.com>
2022-04-17 02:36:46 -0500

    Update getidentifier.lua

--- getidentifier.lua
+++ getidentifier.lua
@@ -6,7 +6,7 @@ if syn then
     identifierheader = "Syn-User-Identifier"
 elseif identifyexecutor():lower():find("scriptware") then
     request = http.request
-    identifierheader = "SW-User-Identifier"
+    identifierheader = "Sw-User-Identifier"
 end
 
 local headers = game:GetService("HttpService"):JSONDecode(request({Url = "https://httpbin.org/get", Method = "GET"}).Body).headers
